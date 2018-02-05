9ef514050c1e79794108069f955b56d62c1195b8
maweizhuo <maweizhuo1314@163.com>
2018-02-05 17:55:52 +0800

    first ci

---
+++ src/Test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gzit000543
+ * Date: 2018/2/5
+ * Time: 下午5:39
+ */
+
+namespace TestCmp;
+
+class Test {
+    public function printHello(){
+        echo "hello world";
+
+    }
+}
